7cd98bf4abce292b13d5083960cde7fa6c81c9db
BuildTools <unconfigured@null.spigotmc.org>
2022-07-08 12:18:33 +0300

    First commit

---
+++ src/main/java/ted_2001/testweight/Testweight.java
@@ -0,0 +1,17 @@
+package ted_2001.testweight;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Testweight extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
