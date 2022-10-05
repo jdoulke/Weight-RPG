975af642242949c2b39afd843d937940d670079f
BuildTools <unconfigured@null.spigotmc.org>
2022-10-05 18:37:28 +0300

    Fix a message if playerweight.get(p.getUniqueID() is null.

--- src/main/java/ted_2001/WeightRPG/Utils/CalculateWeight.java
+++ src/main/java/ted_2001/WeightRPG/Utils/CalculateWeight.java
@@ -164,7 +164,7 @@ public class CalculateWeight {
     }
 
     private boolean isInRegion(Player p) {
-        RegionContainer container = WorldGuard.getInstance().getPlatform().getRegionContainer();
+        /*RegionContainer container = WorldGuard.getInstance().getPlatform().getRegionContainer();
         RegionManager regions = container.get(BukkitAdapter.adapt(p.getWorld()));
         if (regions != null) {
             Location location = p.getLocation();
@@ -179,7 +179,7 @@ public class CalculateWeight {
                     return true;
                 }
             }
-        }
+        }*/
         return false;
     }
 
