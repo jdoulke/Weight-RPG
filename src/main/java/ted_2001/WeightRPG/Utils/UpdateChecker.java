5f6cbaf237ed94e6f1818c75e1d541c0792ab9cb
Ioannis Doulkeridis <jdoulke@gmail.com>
2023-07-17 19:58:14 +0300

    added comments to updateChecker

--- src/main/java/ted_2001/WeightRPG/Utils/UpdateChecker.java
+++ src/main/java/ted_2001/WeightRPG/Utils/UpdateChecker.java
@@ -9,6 +9,9 @@ import java.net.URL;
 import java.util.Scanner;
 import java.util.function.Consumer;
 
+/**
+ * UpdateChecker class responsible for checking plugin updates using Spigot API.
+ */
 public class UpdateChecker {
 
     private final WeightRPG plugin;
@@ -19,13 +22,23 @@ public class UpdateChecker {
         this.resourceId = resourceId;
     }
 
+    /**
+     * Get the latest version of the plugin from the Spigot API asynchronously.
+     * The result will be passed to the provided consumer.
+     *
+     * @param consumer A consumer that accepts the latest version as a String.
+     */
     public void getVersion(final Consumer<String> consumer) {
         Bukkit.getScheduler().runTaskAsynchronously(this.plugin, () -> {
-            try (InputStream inputStream = new URL("https://api.spigotmc.org/legacy/update.php?resource=" + this.resourceId).openStream(); Scanner scanner = new Scanner(inputStream)) {
+            try (InputStream inputStream = new URL("https://api.spigotmc.org/legacy/update.php?resource=" + this.resourceId).openStream();
+                 Scanner scanner = new Scanner(inputStream)) {
+
+                // Check if the response from the API contains version information
                 if (scanner.hasNext()) {
                     consumer.accept(scanner.next());
                 }
             } catch (IOException exception) {
+                // Log an error if unable to check for updates
                 plugin.getLogger().info("Unable to check for updates: " + exception.getMessage());
             }
         });
