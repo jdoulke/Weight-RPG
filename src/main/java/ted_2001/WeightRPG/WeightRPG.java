4a8bfed825bc895083e0de3528f580d46fcfe577
Ioannis Doulkeridis <jdoulke@gmail.com>
2023-07-17 20:06:04 +0300

    Added comments to Main Class

--- src/main/java/ted_2001/WeightRPG/WeightRPG.java
+++ src/main/java/ted_2001/WeightRPG/WeightRPG.java
@@ -24,21 +24,24 @@ import java.util.Objects;
 
 public final class WeightRPG extends JavaPlugin {
 
-
     private static WeightRPG plugin;
-
-    public BukkitScheduler scheduler = this.getServer().getScheduler();
-    public BukkitTask task;
+    private BukkitScheduler scheduler = this.getServer().getScheduler();
+    private BukkitTask task;
     private String pluginPrefix;
 
     @Override
     public void onEnable() {
+
         plugin = this;
         pluginPrefix = ChatColor.translateAlternateColorCodes('&', Objects.requireNonNull(getConfig().getString("plugin-prefix")));
+
+        // Register listeners and commands
         getServer().getPluginManager().registerEvents(new WeightCalculateListeners(), this);
         Objects.requireNonNull(getCommand("weight")).setExecutor(new WeightCommand());
         TabCompleter tc = new Tabcompleter();
         Objects.requireNonNull(getPlugin().getCommand("weight")).setTabCompleter(tc);
+
+        // Initialize JSON files and configuration
         JsonFile js = new JsonFile();
         getServer().getConsoleSender().sendMessage(pluginPrefix + ChatColor.GRAY + "Preparing config and weight files...");
         getConfig().options().copyDefaults();
@@ -51,76 +54,86 @@ public final class WeightRPG extends JavaPlugin {
         js.saveJsonFile();
         Messages.create();
         js.readJsonFile();
-        if(js.successfullyRead) {
-            getServer().getConsoleSender().sendMessage(pluginPrefix + ChatColor.GRAY+"Reading weight files completed" + ChatColor.GREEN + " SUCCESSFULLY.");
-        }else{
-            getServer().getConsoleSender().sendMessage(pluginPrefix + ChatColor.RED + "ERROR" + ChatColor.GRAY+" Weight or Config files have ERROR(s).");
+        if (js.successfullyRead) {
+            getServer().getConsoleSender().sendMessage(pluginPrefix + ChatColor.GRAY + "Reading weight files completed" + ChatColor.GREEN + " SUCCESSFULLY.");
+        } else {
+            getServer().getConsoleSender().sendMessage(pluginPrefix + ChatColor.RED + "ERROR" + ChatColor.GRAY + " Weight or Config files have ERROR(s).");
         }
         getServer().getConsoleSender().sendMessage(pluginPrefix + ChatColor.GRAY + "Done.");
-        // Small check to make sure that PlaceholderAPI is installed
-        if(Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null) {
-            getServer().getConsoleSender().sendMessage(pluginPrefix + ChatColor.AQUA + "PlaceholderAPI" +ChatColor.GRAY + " found. Registering placeholders");
+
+        // Check for PlaceholderAPI and register placeholders
+        if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null) {
+            getServer().getConsoleSender().sendMessage(pluginPrefix + ChatColor.AQUA + "PlaceholderAPI" + ChatColor.GRAY + " found. Registering placeholders");
             new WeightExpasion().register();
             getServer().getConsoleSender().sendMessage(pluginPrefix + ChatColor.GRAY + "Done.");
         }
+
+        // Schedule weight calculation task
         scheduler();
+
+        // Check for plugin updates
         new UpdateChecker(this, 105513).getVersion(version -> {
             if (this.getDescription().getVersion().equals(version)) {
                 getServer().getConsoleSender().sendMessage(pluginPrefix + ChatColor.GRAY + "There is no new update available.");
-            } else if (Double.parseDouble(this.getDescription().getVersion()) < Double.parseDouble(version)){
+            } else if (Double.parseDouble(this.getDescription().getVersion()) < Double.parseDouble(version)) {
                 getServer().getConsoleSender().sendMessage(pluginPrefix + ChatColor.GRAY + "There is a new update available.");
             } else {
-                getServer().getConsoleSender().sendMessage(pluginPrefix + ChatColor.GRAY + "You are using a newer version compared to the version uploaded on spigot.");
+                getServer().getConsoleSender().sendMessage(pluginPrefix + ChatColor.GRAY + "You are using a newer version compared to the version uploaded on Spigot.");
             }
         });
-        Metrics metrics = new Metrics(this,16524);
 
+        // Initialize metrics
+        Metrics metrics = new Metrics(this, 16524);
     }
 
+    /**
+     * Schedules the weight calculation task to run at a configurable interval.
+     */
     public void scheduler() {
         int timer;
-        if(this.getConfig().getDouble("check-weight") <= 0)
+        if (this.getConfig().getDouble("check-weight") <= 0) 
             timer = 2;
-        else
+        else 
             timer = (int) this.getConfig().getDouble("check-weight");
+        
 
         task = scheduler.runTaskTimer(this, () -> {
             List<Player> players = (List<Player>) getPlugin().getServer().getOnlinePlayers();
-            CalculateWeight weightCalculator= new CalculateWeight();
-            for (Player player : players)
-                if(!player.hasPermission("weight.bypass"))
+            CalculateWeight weightCalculator = new CalculateWeight();
+            for (Player player : players) {
+                if (!player.hasPermission("weight.bypass")) {
                     weightCalculator.calculateWeight(player);
-        },0, timer * 20L);
+                }
+            }
+        }, 0, timer * 20L);
     }
 
     @Override
-    public void onLoad(){
-        if(getServer().getPluginManager().getPlugin("WorldGuard") != null) {
+    public void onLoad() {
+        // Check for WorldGuard and initialize WorldGuardRegionHolder if found
+        if (getServer().getPluginManager().getPlugin("WorldGuard") != null) {
             try {
                 WorldGuardRegionHolder holder = new WorldGuardRegionHolder();
                 holder.RegionHolder();
-            } catch (NoClassDefFoundError e) {
-                // Do something here
-            }
+            } catch (NoClassDefFoundError e) {}
         }
     }
+
     @Override
     public void onDisable() {
         saveDefaultConfig();
     }
 
-
-
-
-    public static WeightRPG getPlugin(){
+    public static WeightRPG getPlugin() {
         return plugin;
     }
 
-    public void reloadPluginPrefix(){
+
+    public void reloadPluginPrefix() {
         this.pluginPrefix = ChatColor.translateAlternateColorCodes('&', Objects.requireNonNull(getConfig().getString("plugin-prefix")));
     }
 
-    public String getPluginPrefix(){return pluginPrefix;}
-
-
+    public String getPluginPrefix() {
+        return pluginPrefix;
+    }
 }
