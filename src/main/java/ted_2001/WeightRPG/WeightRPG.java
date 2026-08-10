9437c8643e4b7e8139ad52fdc05bc870b9e28547
Ioannis Doulkeridis <jdoulke@gmail.com>
2026-08-11 02:41:15 +0300

    Route movement events through optimized adapter

--- src/main/java/ted_2001/WeightRPG/WeightRPG.java
+++ src/main/java/ted_2001/WeightRPG/WeightRPG.java
@@ -9,7 +9,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitTask;
 import ted_2001.WeightRPG.Commands.Tabcompleter;
 import ted_2001.WeightRPG.Commands.WeightCommands;
-import ted_2001.WeightRPG.Listeners.WeightCalculateListeners;
+import ted_2001.WeightRPG.Listeners.OptimizedWeightCalculateListener;
 import ted_2001.WeightRPG.Listeners.WeightCommandValidationListener;
 import ted_2001.WeightRPG.Utils.CalculateWeight;
 import ted_2001.WeightRPG.Utils.JsonFile;
@@ -34,7 +34,7 @@ public final class WeightRPG extends JavaPlugin {
         saveDefaultConfig();
         reloadPluginPrefix();
 
-        getServer().getPluginManager().registerEvents(new WeightCalculateListeners(), this);
+        getServer().getPluginManager().registerEvents(new OptimizedWeightCalculateListener(), this);
         getServer().getPluginManager().registerEvents(new WeightCommandValidationListener(), this);
 
         PluginCommand weightCommand = Objects.requireNonNull(getCommand("weight"), "Command 'weight' is missing from plugin.yml");
